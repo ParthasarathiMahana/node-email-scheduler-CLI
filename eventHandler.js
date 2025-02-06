3c23339d92ad8786eefe5f5c01c9daa688d845f4
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2025-02-06 07:04:29 +0530

    custom even is created to append email data to json file.

--- eventHandler.js
+++ eventHandler.js
@@ -1,25 +1,55 @@
 const EventEmitter = require('events')
 const readLine = require('readline')
+const fs = require('fs')
 
 class CustomEvents extends EventEmitter{
-    
-    async getUserInput(){
+    // Event-1(Getting user input from terminal)
+    getUserInput(){
         const commandLineInterface = readLine.createInterface({
             input: process.stdin,
             output: process.stdout
         })
+        
         commandLineInterface.question('to: ', (toEmail)=>{
             commandLineInterface.question('subject: ', (subject)=>{
                 commandLineInterface.question('body: ', (body)=>{
-                    commandLineInterface.close()
-                    this.emit('getUserInput', {toEmail, subject, body})
+                    commandLineInterface.question('scheduledTime in 24hrs format(hh:mm): ', (time)=>{
+                        commandLineInterface.close()
+                        this.emit('getUserInput', {toEmail, subject, body, time})
+                    })
                 })
             })
         })
-
     }
 
-
+    // Event-2(appending the user input in emails.json file)
+    appendData(data){
+        fs.readFile('emails.json', (error, fileData)=>{
+            error && console.log("Error while reading emails.json", error);
+            if(fileData.toString()){
+                let emailDataArray = JSON.parse(fileData)
+                emailDataArray.push(data)
+                this.#addDataToFile(emailDataArray)
+                
+            }else{
+                let emailDataArray = []
+                emailDataArray.push(data)
+                this.#addDataToFile(emailDataArray)
+            }
+        })
+    }
+    // private method to be used inside the class only
+    #addDataToFile(data){
+        let stringifyEmailData = JSON.stringify(data)
+        fs.writeFile('emails.json', stringifyEmailData, 'utf8', (error)=>{
+            if(error){
+                console.log('Error while adding data to email.jason file, ', error);
+            }else{
+                console.log(`Your email is successfully scheduled.`);
+                this.emit('addEmailDetails', {filePath: 'emails.json'})
+            }
+        })
+    }
 }
 
 module.exports = CustomEvents
--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const CustomEvents = require('./eventHandler')
 const customEventObject = new CustomEvents()
 customEventObject.getUserInput()
 
-customEventObject.addListener('getUserInput', (content)=>{
-    console.log('I am the listener ',content);
+customEventObject.addListener('getUserInput', (data)=>{
+    customEventObject.appendData(data)
+})
+
+customEventObject.addListener('addEmailDetails', ({filePath})=>{
+    console.log('file path is: ', filePath);
 })
